d4cc13e9f5b6da49d22422fd3da22f7032a4c937
rohdalvi <43485616+rohdalvi@users.noreply.github.com>
2018-09-28 19:47:55 -0400

    Update Predictor.py

--- Predictor.py
+++ Predictor.py
@@ -1,3 +1,5 @@
+import plotly.plotly as py
+import plotly.graph_objs as go
 print "Welcome to the March Madness Predictor, 1.0"
 print "_______________________"
 print ""
@@ -47,10 +49,51 @@ total = 1*p1 + (-0.5)*p2 + 0.8*p3 + 0.5*p4 + 0.8*p5 + 0.8*p6
 # total > 0 if in favor of t1, < 0 if in favor of t2
 if total > 0:
 	print t1 + " is more likely to win."
-
 elif total < 0:
 	print t2 + " is more likely to win."
 else:
 	print "Both teams are equally likely to win...flip a coin?"
 
-print "\n"    
+print 'Percent diff = ' + str(total)
+
+# plotly graphing
+x = ['PPG', 'OPP PPG', 'ASST', 'RBS', 'FT %', '3PT %']
+y = [ppg1, opp_ppg1, assist1, rebound1, ftp1*100, threep1*100]
+y2 = [ppg2, opp_ppg2, assist2, rebound2, ftp2*100, threep2*100]
+
+trace1 = go.Bar(
+	x=x,
+	y=y,
+	name=t1,
+	text=y,
+	textposition='auto',
+	marker=dict(
+		color='rgb(0, 191, 255)',
+		line=dict(
+			color='rgb(8,48,107)',
+			width=1.5),
+		),
+	opacity=0.6
+)
+
+trace2 = go.Bar(
+	x=x,
+	y=y2,
+	name=t2,
+	text=y2,
+	textposition='auto',
+	marker=dict(
+		color='rgb(255, 127, 80)',
+		line=dict(
+			color='rgb(8,48,107)',
+			width=1.5),
+		),
+	opacity=0.6
+)
+
+data = [trace1, trace2]
+layout = go.Layout(
+	title=t1+' vs '+t2
+)
+py.plot(data, filename='bar_plot')
+print "\n"
